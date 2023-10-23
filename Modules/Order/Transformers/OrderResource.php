d542c56308fd2b0e81b1cd9b08d31054b613cb77
elbiheiry <mohamedsasa201042@yahoo.com>
2023-10-23 20:46:31 +0300

    hot update

--- Modules/Order/Transformers/OrderResource.php
+++ Modules/Order/Transformers/OrderResource.php
@@ -2,6 +2,7 @@
 
 namespace Modules\Order\Transformers;
 
+use Carbon\Carbon;
 use Illuminate\Http\Resources\Json\JsonResource;
 use Modules\User\Transformers\AddressResource;
 
@@ -17,6 +18,9 @@ class OrderResource extends JsonResource
     {
         return [
             'id' => (int) $this->id,
+            'name' => (string) 'طلب بتاريخ : ' .Carbon::parse($this->created_at)->format('d/m/y'),
+            'orders_count' => (int) $this->items->count(),
+            'order_number' => (int) $this->id,
             'address' => new AddressResource($this->address),
             'total' => (double) $this->total,
             'status' => (string) $this->status,
